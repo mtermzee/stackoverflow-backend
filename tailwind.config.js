500de3cda76c290e09ce7a1d7d8516db48d96782
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-09-10 15:23:50 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,ts,jsx,tsx}",
+    "./templates/**/*.{twig,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,13 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // tailwindcss
+    .enablePostCssLoader((options) => {
+        options.postcssOptions = {
+            config: './postcss.config.js',
+        };
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
