e8d1869258d8bd3bdc77b55ae8a3170d70178a35
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-09-13 17:50:03 +0200

    setup the apFixture with the factorys to create fake Data to database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AnswerFactory;
+use App\Factory\QuestionFactory;
+use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +14,33 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        TagFactory::new()->createMany(100);
+
+        // create 20 questions
+        $questions = QuestionFactory::new()->createMany(20, function () {
+            // we user return to rlate defrrent id-tags to defrrent questions
+            return [
+                // realte tags to the 20.questions randomly
+                'tags' => TagFactory::randomRange(0, 5)
+            ];
+        });
+
+        // add unpulished questions
+        QuestionFactory::new()
+            ->unpublished()
+            ->createMany(5);
+
+        AnswerFactory::new(function () use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)]
+            ];
+        })->needsApproval()->many(20)->create();
+
+        AnswerFactory::createMany(100, function () use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)]
+            ];
+        });
 
         $manager->flush();
     }
