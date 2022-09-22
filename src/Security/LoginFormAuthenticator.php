54ea607f63c1d7b15bf0c0d114461f183b13f3eb
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-09-22 14:33:15 +0200

    Authentication Fails

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,17 +2,16 @@
 
 namespace App\Security;
 
-use App\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
@@ -36,15 +35,24 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     public function authenticate(Request $request): Passport
     {
         // get always from the name of the input field
-        $email = $request->request->get('email');
+        $email = $request->request->get('email', "");
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         // if we do not have hashed password, we can use the password field
         //$password = $request->request->get('password');
 
-        /*return new Passport(
-            new UserBadge($email),
+        /*Custom User Query & Credentials
+        return new Passport(
+            new UserBadge($email, function ($userIdentifer) {
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifer]);
+
+                if (!$user) {
+                    throw new UserNotFoundException();
+                }
+
+                return $user;
+            }),
             new CustomCredentials(function ($credentials, User $user) {
                 dd($credentials, $user);
             }, $password),
@@ -52,6 +60,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
                 new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
             ]
         );*/
+
+        //dd('authenticate');
 
         // when we have hashed password
         return new Passport(
@@ -61,8 +71,6 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
             ]
         );
-
-        //dd('authenticate');
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
@@ -75,6 +83,15 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
         return new RedirectResponse($this->urlGenerator->generate('app_question'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        $url = $this->getLoginUrl($request);
+
+        return new RedirectResponse($url);
     }
 
     protected function getLoginUrl(Request $request): string
